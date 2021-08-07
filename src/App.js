9cd5e170ddb8d5fa9406b945420e26833b1a45df
Nathan Conroy <nathan.conroy@protonmail.com>
2021-08-07 14:53:41 -0500

    Remove React default page content.

--- src/App.js
+++ src/App.js
@@ -4,22 +4,19 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
     </div>
   );
+}
+
+function Header() {
+    return (
+      <header className="App-header">
+        <p>
+            Welcome to Nathan Conroy's blog! More content to come.
+        </p>
+      </header>
+    );
 }
 
 export default App;
