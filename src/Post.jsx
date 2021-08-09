94a6de85c0f2e351193140b58f6185f118c73f07
Nathan Conroy <nathan.conroy@protonmail.com>
2021-08-08 22:24:20 -0500

    Add Post.jsx

---
+++ src/Post.jsx
@@ -0,0 +1,19 @@
+
+function Post() {
+  return (
+    <div className="post">
+      <PostHdr />
+      <PostTimeStamp />
+    </div>
+  );
+}
+
+function PostHdr() {
+    return <h2>I am a post!</h2>
+}
+
+function PostTimeStamp() {
+    return <h3>August 7th, 2021</h3>;
+}
+
+export default Post;
