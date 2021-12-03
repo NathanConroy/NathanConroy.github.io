8001bd3ddbb047c875b3b18fcd627c397ac59634
Nathan Conroy <nathan.conroy@protonmail.com>
2021-12-03 13:26:32 -0600

    Add code to fetch posts from backend (do not yet display this content).

--- src/Posts.jsx
+++ src/Posts.jsx
@@ -1,8 +1,34 @@
-import { Fragment } from 'react';
-
+import { Fragment, useState, useEffect } from 'react';
 import Post from './Post';
 
+import axios from 'axios';
+
+const POSTS_URL = 'https://nathanconroydev.pythonanywhere.com/posts';
+
+/*
+ * Fetch posts from the backend.
+ */
+function fetchPosts(setPosts) {
+  return axios.get(POSTS_URL)
+    .then((resp) => resp.data)
+    .then((data) => { console.dir(data); setPosts(data); })
+    .catch(); // TODO: handle exception
+}
+
+/*
+ * Returns lists of posts.
+ */
+function usePosts() {
+  const [posts, setPosts] = useState([]);
+  useEffect(() => {
+    fetchPosts(setPosts);
+  }, [])
+  return posts;
+}
+
+
 function Posts() {
+  usePosts();
   return (
     <>
       <Post />
