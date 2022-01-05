dfef288a65bf249e1ba7fb8aa60dd8654359139a
nconroy <nathanconroydev@gmail.com>
2022-01-04 22:50:48 -0600

    Move PostObj to backend/posts.js

--- src/backend/posts.js
+++ src/backend/posts.js
@@ -5,7 +5,26 @@
 import axios from 'axios';
 
 import { getPostsURL } from './urls';
-import PostObj from '../PostObj';
+
+const dtFmt = new Intl.DateTimeFormat('en-US', { dateStyle: 'full' })
+
+/*
+ * Class that contains all the data related to a single
+ * blog post.
+ */
+class PostObj {
+  constructor(id, title, author, content, date) {
+    this.id = id;
+    this.author = author;
+    this.title = title;
+    this.content = content;
+    this.date = Date.parse(date);
+  }
+
+  get dateStr() {
+    return dtFmt.format(this.date);
+  }
+}
 
 /*
  * Sorts posts by date.
