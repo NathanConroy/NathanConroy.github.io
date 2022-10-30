52d961d184efe9c36e14c66c036bf5ca4fa28f45
Nathan Conroy <nathan.conroy@protonmail.com>
2022-10-29 21:58:09 -0500

    Style posts differently per mobile and other devices

--- src/Post.jsx
+++ src/Post.jsx
@@ -1,9 +1,25 @@
+import { useMediaQuery } from 'react-responsive'
 
 const FIRST_SECTION = 0;
 
+const MOBILE_POST_MOD = 'post--mobile';
+const DEF_POST_MOD = 'post--desktop-laptop-or-tablet';
+
+function postStyleModifier(isMobile) {
+  if (isMobile) {
+    return MOBILE_POST_MOD;
+  }
+  return DEF_POST_MOD;
+}
+
+function postStyle(isMobile) {
+  return `post ${postStyleModifier(isMobile)}`;
+}
+
 function Post({ post }) {
+  const isMobile = useMediaQuery({ maxWidth: 767 })
   return (
-    <div className="post">
+    <div className={postStyle(isMobile)}>
       <PostHdr title={post.title} />
       <PostTimeStamp date={post.dateStr} />
       <PostContent sections={post.sections} />
