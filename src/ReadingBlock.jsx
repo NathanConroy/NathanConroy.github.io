48ffc30e1dbd033df72ed1358e807950f4d56ff1
Nathan Conroy <nathan.conroy@protonmail.com>
2022-10-29 23:08:22 -0500

    Move margin styling over to ReadingBlock

--- src/ReadingBlock.jsx
+++ src/ReadingBlock.jsx
@@ -1,8 +1,27 @@
 /*
  * Container for text-based content.
  */
+import { useMediaQuery } from 'react-responsive'
+
+import './ReadingBlock.css';
+
+const MOBILE_POST_MOD = 'reading-block--mobile';
+const DEF_POST_MOD = 'reading-block--desktop-laptop-or-tablet';
+
 function ReadingBlock(props) {
-  return props.children;
+  const isMobile = useMediaQuery({ maxWidth: 767 })
+  return (
+    <div className={readingBlockStyle(isMobile)}>
+      { props.children }
+    </div>
+  );
+}
+
+function readingBlockStyle(isMobile) {
+  if (isMobile) {
+    return MOBILE_POST_MOD;
+  }
+  return DEF_POST_MOD;
 }
 
 export default ReadingBlock;
